79019af1214240c3772a7101d78946306659dbee
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-08 01:30:45 +0300

    add main class, object and user defined function

--- first.cpp
+++ first.cpp
@@ -1,5 +1,41 @@
 #include<iostream>
-int main(){
-    std::cout<<"hello world!";
+#define PI 3.142 
+
+int r= 2;
+void area();
+
+class MyClass
+{
+   
+   public:
+      int a;
+      void display()
+      {
+         std::cout<<"inside class"<<std::endl;
+      }
+};
+
+int main()
+{
+    MyClass m;
+    m.a = 90;
+    m.display();
+    area();
+    std::cout<<"yah man Raindrop"<<std::endl;
+    std::cout<<m.a<<std::endl;
     return 0;
 }
+
+void area()
+{
+    float area;
+    area=PI*r*r;
+    std::cout<<area<<std::endl;
+}
+
+
+
+
+
+
+
