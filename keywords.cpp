3795caff86be2900ebc872d22c7b415fa28bafc2
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-11 16:40:31 +0300

    add keyword file

---
+++ keywords.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+int main()
+{
+   
+    int main = 95;   
+    cout<<main<<endl;
+    return 0;
+
+    // const std::string str= "Ibrahimovic";
+    // cout<<str<<endl;
+    
+}
