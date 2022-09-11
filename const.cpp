70859fda03b582f1ffe818eabc07ceb09b679707
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-11 15:52:48 +0300

    add constants file

---
+++ const.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+   
+    const int a = 95;   
+    cout<<a<<endl;
+
+    const std::string str= "Ibrahimovic";
+    cout<<str<<endl;
+    
+}
+
