17b0b6fda73fb3b84b072f11db20efada845e697
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-13 01:03:44 +0300

    add integer file

---
+++ integer.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    unsigned short ushort_int =56;
+    signed short signed_short =-6;
+    int a =99999999;
+    long int long_int =456789;
+    long long int ll_int=9996154321;
+    // int8_t int8_variable=2;
+
+
+    cout<<"size of short int "<<sizeof(short int)<<endl;   
+    cout<<"size of int "<<sizeof(int)<<endl;
+    cout<<"size of long "<<sizeof(long)<<endl;   
+    cout<<"size of long long "<<sizeof(long long)<<endl;   
+    // cout<<"size of short int"<<sizeof(int8_t)<<endl;   
+    
+
+    cout<<"unsigned short int "<<ushort_int<<endl;
+    cout<<"signed short int value "<<signed_short<<endl;
+    cout<<"long int "<<long_int<<endl;
+    cout<<"ling long int value "<<ushort_int<<endl;
+    // cout<<"int_8"<<int8_variable<<endl;
+    cout<<"a "<<a<<endl;
+
+    return 0;
+
+    
+    
+}    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
