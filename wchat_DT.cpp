b49a33d84bb21a51409be3020fcb587561e7a296
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-26 06:56:52 +0300

    add wide char file

---
+++ wchat_DT.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    wchar_t ch=L'ć';
+    wcout<<(int)ch<<endl;
+    cout<<sizeof(wchar_t)<<endl;
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
